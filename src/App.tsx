4fb7c65151a53a0063ad0d938737cec388ff1a0a
Jack McConnell <jackmc1122@gmail.com>
2020-04-12 18:55:09 -0700

    Add initial mui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './containers/Login';
+import Register from './containers/Register';
+import Header from './components/Header';
+import Home from './containers/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/Login" component={Login} />
+        <Route path="/Register" component={Register} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
