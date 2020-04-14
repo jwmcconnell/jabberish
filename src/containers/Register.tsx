39294ea10ad5bbf6aa9f77117f26d4d363ce4157
Jack McConnell <jackmc1122@gmail.com>
2020-04-13 21:03:48 -0700

    Get front end auth working

--- src/containers/Register.tsx
+++ src/containers/Register.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import AuthForm from '../components/AuthForm';
+import { signupUser } from '../services/auth-api';
 
 const Register = () => {
-  return <Typography>Register</Typography>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUpdateUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleUpdatePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(username, password);
+    signupUser({ username, password }).then(console.log);
+  };
+
+  return (
+    <>
+      <AuthForm
+        formTitle="Register"
+        handleSubmit={handleSubmit}
+        handleUpdateUsername={handleUpdateUsername}
+        handleUpdatePassword={handleUpdatePassword}
+        errorMessage={''}
+      />
+    </>
+  );
 };
 
 export default Register;
